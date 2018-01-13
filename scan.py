17c446dd0af82e97d5577fdd0c841db432f41586
GymZombie <gymzombie@github>
2018-01-13 11:26:36 -0500

    Abstracting targets

--- scan.py
+++ scan.py
@@ -9,5 +9,5 @@ exec(open("./settings.local").read())
 from tenable_io.client import TenableIOClient
 
 client = TenableIOClient(access_key=tenable_access_key, secret_key=tenable_secret_key)
-scan = client.scan_helper.create(name='Test Scan', text_targets='www.tax.virginia.gov', template='basic')
+scan = client.scan_helper.create(name='Test Scan', text_targets=scan_target, template='basic')
 scan.launch().download('Test_Scan.pdf', scan.histories()[0].history_id)
